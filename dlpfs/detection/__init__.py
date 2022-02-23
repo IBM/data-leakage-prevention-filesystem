bbbd237fa715af020a255c35c9966852054439b3
Stefano <stefano.braghin@gmail.com>
2022-02-23 10:30:21 +0000

    code to be tested

---
+++ dlpfs/detection/__init__.py
@@ -0,0 +1,86 @@
+from typing import Any, Dict, Iterator, List
+
+from ..transformation import compile_transformation
+
+import logging
+
+import re
+
+try:
+    import re2
+except ImportError:
+    import re as re2
+
+
+def compile_rules(ruleSpecs, use_google_re2: bool):
+    logging.info(f'Loading specs from {ruleSpecs}')
+    return [
+        {
+            'rules': [build_rule(r, use_google_re2) for r in rule['pattern']],
+            'transformation': compile_transformation(rule['transformation'])
+        } for rule in ruleSpecs['rules']
+    ]
+
+
+def _build_terms(terms: List[str]):
+    return [t.strip() for t in terms]
+
+
+def _build_regex(terms: List[str]):
+    return '|'.join([f'({term})' for term in terms])
+
+
+class LookUpMatcher():
+    def __init__(self, index, term):
+        self.index = index
+        self.term = term
+
+    def start(self) -> int:
+        return self.index
+
+    def end(self) -> int:
+        return self.index + len(self.term)
+
+    def group(self, _: int = 0) -> str:
+        return self.term
+
+
+class LookUp():
+    def __init__(self, terms):
+        self.terms = _build_terms(terms)
+
+    def finditer(self, text: str) -> Iterator[LookUpMatcher]:
+        for term in self.terms:
+            last_index = -1
+            while True:
+                last_index = text.find(term, last_index + 1)
+                if -1 == last_index:
+                    break
+                yield LookUpMatcher(last_index, term)
+
+    def sub(self, fun, text: str) -> str:
+        for m in self.finditer(text):
+            # logging.info('match: {m}')
+            text = text[0:m.start()] + fun(m) + text[m.end():]
+        return text
+
+
+def build_rule(rule_spec: Dict[str, Any], use_google_re2: bool):
+    t = rule_spec['type']
+    spec = rule_spec['spec']
+
+    if t == 're' or t == 're2':
+        if use_google_re2:
+            return re2.compile(spec)
+        else:
+            return re.compile(spec)
+
+    if t == 'lookup':
+        return LookUp(spec)
+    if t == 'lookup2':
+        if use_google_re2:
+            return re2.compile(_build_regex(spec))
+        else:
+            return re.compile(_build_regex(spec))
+
+    raise Exception(f'Unknown pattern type {t}')
---
+++ dlpfs/transformation/__init__.py
@@ -0,0 +1,57 @@
+from typing import Dict, Optional
+from diffprivlib.mechanisms import Binary, Exponential, Gaussian, Laplace
+
+
+def _build_mechanism(transformation: Dict):
+    mech_type = transformation.get('mech', 'Laplace')
+
+    if mech_type == 'Laplace':
+        mech = Laplace(
+            epsilon=transformation.get('e', 0.1),
+            delta=transformation.get('d', 0.0),
+            sensitivity=1
+        )
+
+        return lambda s: str(mech.randomise(float(s)))
+    if mech_type == 'Binary':
+        mech = Binary(
+            epsilon=transformation.get('e', 0.1),
+            value0=transformation.get('true', 0),
+            value1=transformation.get('false', 0),
+        )
+        return lambda s: mech.randomise(s)
+    if mech_type == 'Gaussian':
+        mech = Gaussian(
+            epsilon=transformation.get('e', 0.1),
+            delta=transformation.get('d', 0.0),
+            sensitivity=transformation.get('sensitivity', 1)
+        )
+        return lambda s: str(mech.randomise(float(s)))
+    if mech_type == 'Exponential':
+        mech = Exponential(
+            epsilon=transformation.get('e', 0.1),
+            sensitivity=transformation.get('sensitivy', 1),
+            candidates=transformation.get('candidates', None)
+        )
+        return lambda s: mech.randomise(s)
+
+    raise Exception(f'Unsupported mechanism type: {mech_type}')
+
+
+def _build_redact(transformation: Dict):
+    char = transformation.get('char', '*')
+    return lambda s: char * len(s)
+
+
+def compile_transformation(transformation: Optional[Dict]):
+    if transformation:
+        label = transformation['type'].lower() if transformation['type'] else None
+
+        if label == 'redact':
+            return _build_redact(transformation)
+        if label == 'dp':
+            mechanism = _build_mechanism(transformation)
+            return mechanism
+        if label == 'none':
+            return lambda x: x
+    return lambda s: '*' * len(s)
