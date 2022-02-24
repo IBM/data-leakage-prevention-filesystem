ca887da97790994437d793f370f8e34cb72d36a0
Stefano <stefano.braghin@gmail.com>
2022-02-24 11:24:51 +0000

    update travis & explicit dependencies
    Signed-off-by: Stefano Braghin <527806+stefano81@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -9,34 +9,28 @@ with open(path.join(here, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
 setup(
-    name="dlpfs",  # Required
-    version="0.1.0",  # Required
-    description="An example of Data Leakage Prevention",  # Optional
-    long_description=long_description,  # Optional
-    long_description_content_type="text/markdown",  # Optional (see note above)
-    url="https://github.com/IBM/data-leakage-prevention-file-system",  # Optional
-    author="Stefano Braghin",  # Optional
-    author_email="stefanob@ie.ibm.com",  # Optional
-    classifiers=[  # Optional
+    name="dlpfs",
+    version="0.1.0",
+    description="An example of Data Leakage Prevention",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/IBM/data-leakage-prevention-file-system",
+    author="Stefano Braghin",
+    author_email="stefanob@ie.ibm.com",
+    classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3.8",
     ],
     packages=find_packages(exclude=["contrib", "docs", "tests", "scripts", "build"]),
-    python_requires=">=3.0",
+    python_requires=">=3.8",
     install_requires=[
         "diffprivlib==0.5.1",
         "fusepy==3.0.1",
-        "joblib==1.1.0; python_version >= '3.6'",
-        "numpy==1.22.2; python_version >= '3.8'",
-        "pyre2==0.3.6",
-        "scikit-learn==1.0.2; python_version >= '3.7'",
-        "scipy==1.8.0; python_version < '3.11' and python_version >= '3.8'",
-        "setuptools==60.9.3; python_version >= '3.7'",
-        "threadpoolctl==3.1.0; python_version >= '3.6'",
-    ],  # Optional
-    project_urls={  # Optional
+        "pyre2==0.3.6"
+    ],
+    project_urls={
         "Bug Reports": "https://github.com/IBM/data-leakage-prevention-file-system/issues",
         "Source": "https://github.com/IBM/data-leakage-prevention-file-system",
     },
